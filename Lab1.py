5ed2447a0634984135e29957679ac59683537a4c
Felipe Aguilera <pipo@MacBook-Air-de-Felipe-3.local>
2021-03-22 13:31:26 -0300

    second commit

--- Lab1.py
+++ Lab1.py
@@ -5,3 +5,38 @@ Created on Mon Mar 22 12:40:25 2021
 
 @author: pipo
 """
+import numpy as np
+
+n_cartas = int(input("Cuantas cartas quieres jugar? "))
+lista1 = []
+lista2 = []
+for i in range(n_cartas):
+    n = i+1
+    lista1.append(n)
+    lista2.append(n)
+
+longitudTablero = n_cartas #ancho por alto del tablero
+tablero = []    
+
+def crear_tablero():
+    for i in range(longitudTablero):
+        linea = []
+        for j in range(longitudTablero):
+            linea.append("*")        
+        tablero.append(linea)     
+        
+def imprimir_tablero():
+    for i in tablero:   #for para lineas
+        for j in i:   #for para columnas
+            print(j, end=" ")
+        print()  #cambiar de linea
+    print()
+            
+    
+    
+    
+    
+    
+    
+crear_tablero()
+imprimir_tablero()
